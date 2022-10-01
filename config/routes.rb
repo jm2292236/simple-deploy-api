6e263c36912bb0c2109205a3385ef5c9e7ed5c4d
Jaime Montes de Oca <jaime0422@live.com>
2022-10-01 10:39:59 -0700

    Just a push

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :properties
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+    resources :properties
+    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-  root "properties#index"
+    # Defines the root path route ("/")
+    # root "articles#index"
+    root "properties#index"
 end
